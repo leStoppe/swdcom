29f5cf91da2a1ac711b0467fccf40d6c136bbb1e
lestoppe <lestoppe@gmail.com>
2023-07-16 12:08:36 +0530

    handles comments and paranthesis mixed inline with code

--- swdcomm_preprocessor.py
+++ swdcomm_preprocessor.py
@@ -9,6 +9,12 @@ r_constants = re.compile(r"([a-zA-Z0-9_\$\#]+)\s+equ\s+([a-zA-Z0-9_]+)")
 kDictionary = {}
 # line skips. If the parser sees a line starting with this, the line will be removed
 r_skips = re.compile(r"^\\|\/|\(")
+# matches for comments and parentheses mixed with code
+r_strip_parn = re.compile (r"\(\s+[a-zA-Z0-9_\-\(\s]*\s+\)")
+r_strip_slash= re.compile (r"\/\s+.*\n")
+# matches for comments and parentheses on their own line
+r_strip_parn_full = re.compile (r"\s*\(.*\)(\r|\n)")
+r_strip_slash_full= re.compile (r"\s*\/\s+.*(\r|\n)")
 
 def extract_constants (filename):
     kDictionary = {}
@@ -23,6 +29,35 @@ def extract_constants (filename):
     return kDictionary
 
 def process_forth_source (sourcefile, constants):
+    outputfile = sourcefile + ".swp"
+    fh_in = open(sourcefile, 'r')
+    fh_out = open(outputfile, 'w')
+    for line in fh_in:
+        #line = r_strip_slash_full.sub('', line)
+        #line = r_strip_parn_full.sub('', line)
+        line = r_strip_parn.sub('', line)
+        line = r_strip_slash.sub('', line)
+        if ( len(line) <= 1 ):
+            continue
+        print (line)
+        print (len(line) )
+
+        split_line = re.split("\s+", line)
+        processed_line = ''
+        for word in split_line:
+            if (word in constants.keys() ):
+                word = constants[word]
+            processed_line = processed_line + word + " "
+        fh_out.write(processed_line + "\n")
+    fh_in.close()
+    fh_out.close()
+    originalbackup = sourcefile + ".bak"
+    if (os.path.exists(originalbackup) ):
+        os.remove (originalbackup)
+    os.rename(sourcefile, originalbackup)
+    os.rename(outputfile, sourcefile)
+
+def process_forth_source_org (sourcefile, constants):
     outputfile = sourcefile + ".swp"
     fh_in = open(sourcefile, 'r')
     fh_out = open(outputfile, 'w')
